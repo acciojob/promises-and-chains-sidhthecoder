b870243f6a4c86515aa06bf161436689b013a931
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-03-17 13:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+var btn=document.getElementId("btn");
+var name=document.getElementId("name").value;
+var age=document.getElementId("age").value;
+
+btn.addEventListener('click',function myPromise((event){
+	event.preventDefault();
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>18){
+				resolve("Welcome, "+name+". You can vote.");
+			}
+			else{
+				reject("Oh sorry "+<name>+". You aren't old enough.");
+			}
+		},4000);
+	});
+});
