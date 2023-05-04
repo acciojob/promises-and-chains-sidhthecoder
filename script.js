dde2b4c24063218c2c080cd80f1a3ee067f3c2b8
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-05-04 11:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,29 @@
-const res = document.getElementById("output");
+const form = document.querySelector('#myForm');
+const name = document.querySelector('#name');
+const age = document.querySelector('#age');
+const btn = document.querySelector('#btn');
 
-const promises = [
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 1", time: time / 1000 }), time);
-  }),
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 2", time: time / 1000 }), time);
-  }),
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 3", time: time / 1000 }), time);
-  }),
-];
+form.addEventListener('onsubmit', (event) => {
+	event.preventDefault();
+	
+	if(name.value === '' || age.value === '') {
+		alert('Please fill out all fiels.');
+		return;
+	}
 
-async function callFns() {
-  const start = new Date();
-  // Use Promise.all to wait for all Promises to resolve
-  res.innerHTML += `
-            <tr id="loading">
-                <td colspan=2>Loading...</td>
-            </tr>
-          `;
-  await Promise.all(promises)
-    .then((results) => {
-      res.innerHTML = ``;
-      // Log the array of results
-      results.forEach((e) => {
-        res.innerHTML += `
-            <tr>
-                <td>${e.name}</td>
-                <td>${e.time}</td>
-            </tr>
-          `;
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+	const age = parseInt(age.value);
 
-  const end = new Date();
-  const timeInMillis = end - start;
-  res.innerHTML += `
-            <tr>
-                <td>Total</td>
-                <td>${timeInMillis / 1000}</td>
-            </tr>
-          `;
-}
-callFns();
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age >= 18) resolve();
+			reject();
+		}, 4 * 1000);
+	});
+
+	promise.then(() => {
+		alert(`Welcome, ${name.value}. You can vote.`);
+	})
+	.catch(() => {
+		alert(`Welcome, ${name.value}. You aren't old enough.`);
+	});
+});
